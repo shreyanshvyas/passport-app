052d5ff99f06f86ece03c66eea404430c2efecda
shreyanshvyas <vyasshreyansh71@gmail.com>
2021-03-01 21:23:23 +0530

    Updated

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+
+// passport config 
+require('./config/passport')(passport);
+
+// DB Config
+const db = require('./config/keys').MongoURI;
+
+// Connect to Mongo
+mongoose.connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('MongoDB Connected..'))
+    .catch(err => (console.log(err)));
+
+// Bodyparser
+app.use(express.urlencoded({
+    extended: false
+}));
+
+// Express session
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+// EJs
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+// Routes 
+app.use('/', require('./routes/index'));
+app.use('/users', require('./routes/users'));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server started on port ${PORT}`));
